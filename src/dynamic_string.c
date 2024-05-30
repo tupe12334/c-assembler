78b9bcc44e45d578258b0036cb39474d4660a149
tupe12334 <ofek.gabay.he@gmail.com>
2024-05-30 18:41:07 +0300

    fix: collect all lines for new am file

---
+++ src/dynamic_string.c
@@ -0,0 +1,32 @@
+#include "../include/dynamic_string.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void initDynamicString(DynamicString *dstr) {
+  dstr->size = 1;
+  puts("dsaflkhlkadsfhlk0");
+  dstr->str = (char *)malloc(dstr->size);
+  if (dstr->str == NULL) {
+    fprintf(stderr, "Memory allocation failed\n");
+    exit(1);
+  }
+  dstr->str[0] = '\0';
+}
+
+void strcatDynamicString(DynamicString *dstr, const char *newStr) {
+  size_t newLen = strlen(newStr);
+  dstr->size += newLen; // Increase the size
+  dstr->str = (char *)realloc(dstr->str, dstr->size);
+  if (dstr->str == NULL) {
+    fprintf(stderr, "Memory allocation failed\n");
+    exit(1);
+  }
+  strcat(dstr->str, newStr);
+}
+
+void freeDynamicString(DynamicString *dstr) {
+  free(dstr->str);
+  dstr->str = NULL;
+  dstr->size = 0;
+}
--- src/macros_handler.c
+++ src/macros_handler.c
@@ -1,5 +1,6 @@
 #include "../include/constants.h"
 #include "../include/dictionary.h"
+#include "../include/dynamic_string.h"
 #include "../include/macro_utils.h"
 #include "../include/parser.h"
 #include "../include/utils.h"
@@ -26,37 +27,36 @@ void handle_recording(char **macro_recording, ParsedLine *parsed_line,
   }
 
   dictionary_value = lookup(dictionary, *macro_recording);
-
   line_length = safe_strlen(parsed_line->line);
   current_dictionary_value_length = safe_strlen(dictionary_value);
 
   new_length = line_length + current_dictionary_value_length + 1;
-
   combine_values = (char *)malloc(new_length * sizeof(char));
   if (dictionary_value != NULL) {
     strcpy(combine_values, dictionary_value);
+    strcat(combine_values, "\n");
   }
-  strcpy(combine_values, parsed_line->line);
-
+  strcat(combine_values, parsed_line->line);
   insert(dictionary, *macro_recording, combine_values);
+  free(dictionary_value);
+  free(combine_values);
 }
 
 void macros_handler(FILE *assembly_file) {
   ParsedLine *parsed_line;
   char *macro_recording = NULL;
   Dictionary *dictionary = create_dictionary();
-
+  DynamicString *am_file_content = malloc(sizeof(DynamicString));
+  initDynamicString(am_file_content);
   /* Line counter */
   unsigned long int LC = 0;
 
   char line[MAX_LINE_LENGTH];
 
   while (fgets(line, sizeof(line), assembly_file)) {
-    ParsedLine *temp_parsed_line_list;
     incase_line_counter(&LC);
 
     parsed_line = parse_line(line);
-    temp_parsed_line_list = NULL;
     if (is_macro_declaration_start(parsed_line->type) == 0) {
       macro_recording = parsed_line->value;
       printf("Enter macro_recording in line %lu\n", LC);
@@ -72,14 +72,19 @@ void macros_handler(FILE *assembly_file) {
       char *macr_value = lookup(dictionary, parsed_line->type);
       if (macr_value != NULL) {
         printf("Found a macr use in line %lu\n", LC);
+        strcatDynamicString(am_file_content, macr_value);
+        strcatDynamicString(am_file_content, "\n");
         continue;
       }
       printf("Unknown operator in line %lu\n", LC);
-      continue;
+      exit(EXIT_FAILURE);
     }
-    free(temp_parsed_line_list);
+    strcatDynamicString(am_file_content, parsed_line->line);
+    strcatDynamicString(am_file_content, "\n");
   }
+  puts(am_file_content->str);
 
   free_dictionary(dictionary);
+  freeDynamicString(am_file_content);
   puts("Finished macros_handler");
 }
