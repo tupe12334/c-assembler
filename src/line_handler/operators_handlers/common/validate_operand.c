daffb2209671fe8b570ab1346c41b3aa7f984eec
tupe12334 <ofek.gabay.he@gmail.com>
2024-07-10 18:51:33 +0300

    refactor: Optimize validation of operands in operator handlers

--- src/line_handler/operators_handlers/common/validate_operand.c
+++ src/line_handler/operators_handlers/common/validate_operand.c
@@ -6,30 +6,26 @@
 
 void validate_operand(Operand *operand,
                       unsigned short int valid_address_codes[]) {
-  int length = 0;
-  while (valid_address_codes[length] != NULL) {
-    printf("%d\n", valid_address_codes[length]);
-    length++;
+  int i = 0;
+  while (valid_address_codes[i] >= 0 && valid_address_codes[i] <= 3) {
+    // printf("[%d]: %d\n", i, valid_address_codes[i]);
+    i++;
   }
-  bool is_have_options = length > 0;
-
-  printf("\n%d\n", is_have_options);
-  puts(operand->value);
+  bool is_have_options = i > 0;
   if (operand != NULL && is_have_options == false) {
     fprintf(stderr, "Error: operand is not allowed for operation\n");
     exit(EXIT_FAILURE);
   }
-  printf("Start to validate operand %s\n", operand->value);
-  printf("Operand is valid\n");
+  // printf("Operand is valid\n");
 }
 void validate_operands(OperatorLine *operator_line,
                        unsigned short int valid_src_address_codes[],
                        unsigned short int valid_dst_address_codes[]) {
 
-  printf("Start to validate operands for line %s\n",
-         operator_line->parsed_line->tokens.line);
+  // printf("Start to validate operands for line %s\n",
+  //        operator_line->parsed_line->tokens.line);
   validate_operand(operator_line->operand_dst, valid_dst_address_codes);
-  puts("Finish to validate destination operand");
+  // puts("Finish to validate destination operand");
   validate_operand(operator_line->operand_src, valid_src_address_codes);
-  puts("Finish to validate source operand");
+  // puts("Finish to validate source operand");
 }
--- src/line_handler/operators_handlers/prn_handler.c
+++ src/line_handler/operators_handlers/prn_handler.c
@@ -6,14 +6,14 @@
 #include <stdio.h>
 
 void valid_prn_operands(OperatorLine *operator_line) {
-  unsigned short int valid_src_address_codes[0];
+  unsigned short int valid_src_address_codes[] = {-1};
   unsigned short int valid_dst_address_codes[] = {0, 1, 2, 3};
   validate_operands(operator_line, valid_src_address_codes,
                     valid_dst_address_codes);
 }
 
 void prn_operator_handler(DynamicArray *program, OperatorLine *operator_line) {
-  // valid_prn_operands(operator_line);
+  valid_prn_operands(operator_line);
   int binary_code = common_handler(program, operator_line);
   unsigned short op_code = operator_line->opcode;
   int op_code_binary_section = op_code << 11;
