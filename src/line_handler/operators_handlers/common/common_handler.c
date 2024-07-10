8b851c1a34a9303424bb1ac314cf91dd2c32f010
tupe12334 <ofek.gabay.he@gmail.com>
2024-07-10 18:51:18 +0300

    refactor: Optimize binary code generation in common_handler

--- src/line_handler/operators_handlers/common/common_handler.c
+++ src/line_handler/operators_handlers/common/common_handler.c
@@ -4,6 +4,21 @@
 #include <stdio.h>
 
 int common_handler(DynamicArray *program, OperatorLine *operator_line) {
+  int binary_code = 0;
   int instruction_binary_code = 4;
+  binary_code += instruction_binary_code;
+
+  if (operator_line != NULL && operator_line->operand_dst != NULL &&
+      operator_line->operand_dst->address_type != NULL) {
+    int dst_address_binary_section = operator_line->operand_dst->address_type
+                                     << 3;
+    binary_code += dst_address_binary_section;
+  }
+  if (operator_line != NULL && operator_line->operand_src != NULL &&
+      operator_line->operand_src->address_type != NULL) {
+    int src_address_binary_section = operator_line->operand_src->address_type
+                                     << 7;
+    binary_code += src_address_binary_section;
+  }
   return instruction_binary_code;
 }
--- src/line_handler/operators_handlers/prn_handler.c
+++ src/line_handler/operators_handlers/prn_handler.c
@@ -17,19 +17,6 @@ void prn_operator_handler(DynamicArray *program, OperatorLine *operator_line) {
   int binary_code = common_handler(program, operator_line);
   unsigned short op_code = operator_line->opcode;
   int op_code_binary_section = op_code << 11;
-  if (operator_line != NULL && operator_line->operand_dst != NULL &&
-      operator_line->operand_dst->address_type != NULL) {
-    binary_code += op_code_binary_section;
-    int dst_address_binary_section = operator_line->operand_dst->address_type
-                                     << 3;
-    binary_code += dst_address_binary_section;
-  }
-  if (operator_line != NULL && operator_line->operand_src != NULL &&
-      operator_line->operand_src->address_type != NULL) {
-    int src_address_binary_section = operator_line->operand_src->address_type
-                                     << 7;
-    binary_code += src_address_binary_section;
-  }
 
   darray_append(program, binary_code, false);
 }
