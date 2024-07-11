2181d670a67445b2e28ddb0425549330e3cd79ec
tupe12334 <ofek.gabay.he@gmail.com>
2024-07-11 16:47:39 +0300

    refactor: Add valid_mov_operands, valid_cmp_operands, valid_sub_operands, valid_lea_operands, valid_clr_operands, valid_not_operands, valid_inc_operands, valid_dec_operands, valid_jmp_operands, valid_bne_operands, valid_red_operands, valid_prn_operands, valid_jsr_operands, valid_rts_operands, and valid_stop_operands functions for validating operands in operator handlers

--- src/line_handler/operators_handlers/operator_validators.c
+++ src/line_handler/operators_handlers/operator_validators.c
@@ -1,14 +1,75 @@
 #include "../../../include/operator_validators.h"
 #include "../../../include/line.h"
 
-void valid_jsr_operands(OperatorLine *operator_line) {
-  unsigned short int valid_src_address_codes[] = {-1};
-  unsigned short int valid_dst_address_codes[] = {1, 2};
+void valid_mov_operands(OperatorLine *operator_line) {
+  unsigned short int valid_src_address_codes[] = {0, 1, 2, 3};
+  unsigned short int valid_dst_address_codes[] = {1, 2, 3};
+  validate_operands(operator_line, valid_src_address_codes,
+                    valid_dst_address_codes);
+};
+void valid_cmp_operands(OperatorLine *operator_line) {
+  unsigned short int valid_src_address_codes[] = {0, 1, 2, 3};
+  unsigned short int valid_dst_address_codes[] = {0, 1, 2, 3};
   validate_operands(operator_line, valid_src_address_codes,
                     valid_dst_address_codes);
 };
 void valid_add_operands(OperatorLine *operator_line) {
   unsigned short int valid_src_address_codes[] = {0, 1, 2, 3};
+  unsigned short int valid_dst_address_codes[] = {1, 2, 3};
+  validate_operands(operator_line, valid_src_address_codes,
+                    valid_dst_address_codes);
+};
+void valid_sub_operands(OperatorLine *operator_line) {
+  unsigned short int valid_src_address_codes[] = {0, 1, 2, 3};
+  unsigned short int valid_dst_address_codes[] = {1, 2, 3};
+  validate_operands(operator_line, valid_src_address_codes,
+                    valid_dst_address_codes);
+};
+void valid_lea_operands(OperatorLine *operator_line) {
+  unsigned short int valid_src_address_codes[] = {1};
+  unsigned short int valid_dst_address_codes[] = {1, 2, 3};
+  validate_operands(operator_line, valid_src_address_codes,
+                    valid_dst_address_codes);
+};
+
+void valid_clr_operands(OperatorLine *operator_line) {
+  unsigned short int valid_src_address_codes[] = {-1};
+  unsigned short int valid_dst_address_codes[] = {1, 2, 3};
+  validate_operands(operator_line, valid_src_address_codes,
+                    valid_dst_address_codes);
+};
+void valid_not_operands(OperatorLine *operator_line) {
+  unsigned short int valid_src_address_codes[] = {-1};
+  unsigned short int valid_dst_address_codes[] = {1, 2, 3};
+  validate_operands(operator_line, valid_src_address_codes,
+                    valid_dst_address_codes);
+};
+void valid_inc_operands(OperatorLine *operator_line) {
+  unsigned short int valid_src_address_codes[] = {-1};
+  unsigned short int valid_dst_address_codes[] = {1, 2, 3};
+  validate_operands(operator_line, valid_src_address_codes,
+                    valid_dst_address_codes);
+};
+void valid_dec_operands(OperatorLine *operator_line) {
+  unsigned short int valid_src_address_codes[] = {-1};
+  unsigned short int valid_dst_address_codes[] = {1, 2, 3};
+  validate_operands(operator_line, valid_src_address_codes,
+                    valid_dst_address_codes);
+};
+void valid_jmp_operands(OperatorLine *operator_line) {
+  unsigned short int valid_src_address_codes[] = {-1};
+  unsigned short int valid_dst_address_codes[] = {1, 2};
+  validate_operands(operator_line, valid_src_address_codes,
+                    valid_dst_address_codes);
+};
+void valid_bne_operands(OperatorLine *operator_line) {
+  unsigned short int valid_src_address_codes[] = {-1};
+  unsigned short int valid_dst_address_codes[] = {1, 2};
+  validate_operands(operator_line, valid_src_address_codes,
+                    valid_dst_address_codes);
+};
+void valid_red_operands(OperatorLine *operator_line) {
+  unsigned short int valid_src_address_codes[] = {-1};
   unsigned short int valid_dst_address_codes[] = {1, 2, 3};
   validate_operands(operator_line, valid_src_address_codes,
                     valid_dst_address_codes);
@@ -19,9 +80,21 @@ void valid_prn_operands(OperatorLine *operator_line) {
   validate_operands(operator_line, valid_src_address_codes,
                     valid_dst_address_codes);
 };
-void valid_lea_operands(OperatorLine *operator_line) {
-  unsigned short int valid_src_address_codes[] = {1};
-  unsigned short int valid_dst_address_codes[] = {1, 2, 3};
+void valid_jsr_operands(OperatorLine *operator_line) {
+  unsigned short int valid_src_address_codes[] = {-1};
+  unsigned short int valid_dst_address_codes[] = {1, 2};
+  validate_operands(operator_line, valid_src_address_codes,
+                    valid_dst_address_codes);
+};
+void valid_rts_operands(OperatorLine *operator_line) {
+  unsigned short int valid_src_address_codes[] = {-1};
+  unsigned short int valid_dst_address_codes[] = {-1};
+  validate_operands(operator_line, valid_src_address_codes,
+                    valid_dst_address_codes);
+};
+void valid_stop_operands(OperatorLine *operator_line) {
+  unsigned short int valid_src_address_codes[] = {-1};
+  unsigned short int valid_dst_address_codes[] = {-1};
   validate_operands(operator_line, valid_src_address_codes,
                     valid_dst_address_codes);
 };
