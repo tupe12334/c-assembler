467a46b8fadf9e2d4fb88852d60e99511858614a
tupe12334 <ofek.gabay.he@gmail.com>
2024-07-11 18:22:23 +0300

    refactor: Remove commented debug statements and unused code in operator handlers

---
+++ src/line_handler/instructions_handlers/handle_instruction.c
@@ -0,0 +1,3 @@
+#include "../../../include/dynamic_array.h"
+#include "../../../include/line.h"
+void handle_instructions(DynamicArray *program, ParsedLine *parsed_line) {}
--- src/line_handler/operators_handlers/common/number_handler.c
+++ src/line_handler/operators_handlers/common/number_handler.c
@@ -7,7 +7,7 @@
 int handle_number_operand(Operand *operand, enum OperandSide side) {
   char *value = operand->value;
   char *trimmed_value = ltrim_x_chars(value, 1);
-  printf("The trimed_value: %s\n", trimmed_value);
+  // printf("The trimed_value: %s\n", trimmed_value);
   int num_value = atoi(trimmed_value) << 3;
 
   return num_value;
