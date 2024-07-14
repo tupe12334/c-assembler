863fb4c3cb85742628453c653e827742aab18923
tupe12334 <ofek.gabay.he@gmail.com>
2024-07-14 03:12:02 +0300

    feat: add handle string support

---
+++ src/line_handler/instructions_handlers/handle_string.c
@@ -0,0 +1,16 @@
+#include "../../../include/dynamic_array.h"
+#include "../../../include/line.h"
+#include "../../../include/string.h"
+#include <stdio.h>
+#include <string.h>
+
+void handle_string(DynamicArray *program, ParsedLine *parsed_line) {
+  char *value = trim(parsed_line->tokens.value);
+  int length = strlen(value);
+  int i;
+  for (i = 1; i < length - 1; i++) {
+    darray_append(program, value[i], true);
+  }
+
+  darray_append(program, 0, true);
+}
